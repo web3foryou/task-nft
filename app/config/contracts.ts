1fb14fe22e00eb6261e09045973a89655980ffab
web3foryou <94456349+web3foryou@users.noreply.github.com>
2022-04-08 01:03:41 +0300

    upd

--- app/config/contracts.ts
+++ app/config/contracts.ts
@@ -2,19 +2,19 @@ export class Contracts {
     NFT: string;
     NFT721: string;
     NFT1155: string;
+    MARKETPLACE: string;
 
     constructor(network :string) {
         this.NFT = process.env.CONTRACT_NFT as string;
         this.NFT721 = process.env.CONTRACT_NFT721 as string;
         this.NFT1155 = process.env.CONTRACT_NFT1155 as string;
+        this.MARKETPLACE = process.env.CONTRACT_MARKETPLACE as string;
+
         if (network == "rinkeby") {
             this.NFT = process.env.CONTRACT_NFT_RINKEBY as string;
             this.NFT721 = process.env.CONTRACT_NFT721_RINKEBY as string;
             this.NFT1155 = process.env.CONTRACT_NFT1155_RINKEBY as string;
-        } else if (network == "ropsten") {
-            this.NFT = process.env.CONTRACT_NFT_ROPSTEN as string;
-            this.NFT721 = process.env.CONTRACT_NFT721_ROPSTEN as string;
-            this.NFT1155 = process.env.CONTRACT_NFT1155_ROPSTEN as string;
+            this.MARKETPLACE = process.env.CONTRACT_MARKETPLACE_RINKEBY as string;
         }
     }
 }
