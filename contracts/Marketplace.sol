a677c3c43a0b49ce225e07f5e460c72689ee7868
web3foryou <94456349+web3foryou@users.noreply.github.com>
2022-04-12 12:53:10 +0300

    refactoring

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,39 +1,39 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./NFT721.sol";
 import "./ERC20Token.sol";
 import "hardhat/console.sol";
 
 
-contract Marketplace is AccessControl {
+contract Marketplace is Ownable {
     address private _nft;
     address private _token;
     uint private _minAuctionTime;
     uint private _minBids;
 
-    struct DataStruct {
-        mapping(uint => uint) availableToBuy; // nftId => price
-        mapping(uint => bool) auctionSw; // nftId => bool - включен акцион или нет
-        mapping(uint => uint) auctionPrice; // nftId => price
-        mapping(uint => uint) auctionBids; // nftId => bids
-        mapping(uint => uint) auctionStartTime; // nftId => StartTime
-        mapping(uint => address) auctionLastBider; // nftId => последний ставочник
+    enum TypeItem { NONE, BUY, AUCTION }
+
+    struct Item {
+        TypeItem typeItem; // тип айтема
+        address  maxBider; // последний участник ставки
+        uint price; // цена продажи / цена последней ставки на аукционе
+        uint maxBid; // число ставок
+        uint stratTime; // время начала аукциона
     }
 
-    DataStruct private data;
+    mapping(uint => Item) private data;
 
     constructor(address nft, address token, uint minAuctionTime, uint minBids) {
         _nft = nft;
         _token = token;
         _minAuctionTime = minAuctionTime;
         _minBids = minBids;
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function createItem(address to, string memory metadata) public onlyMember {
+    function createItem(address to, string memory metadata) public onlyOwner {
         NFT721(_nft).mint(to, metadata);
     }
 
@@ -41,20 +41,22 @@ contract Marketplace is AccessControl {
         require(NFT721(_nft).ownerOf(nftId) == msg.sender, "Not owner.");
         require(NFT721(_nft).getApproved(nftId) == address(this), "Not approved.");
         require(price > 0, "Bad price.");
-        data.availableToBuy[nftId] = price;
+        data[nftId].price = price;
+        data[nftId].typeItem = TypeItem.BUY;
     }
 
     function buyItem(uint nftId) public payable {
         address owner = NFT721(_nft).ownerOf(nftId);
 
         require(NFT721(_nft).getApproved(nftId) == address(this), "Not approved.");
-        require(data.availableToBuy[nftId] > 0, "Not available to buy");
+        require(data[nftId].price > 0, "Not available to buy");
         require(owner != msg.sender, "Already the owner.");
-        require(msg.value >= data.availableToBuy[nftId], "Bad amount.");
+        require(msg.value >= data[nftId].price, "Bad amount.");
 
         NFT721(_nft).safeTransferFrom(owner, msg.sender, nftId);
 
-        data.availableToBuy[nftId] = 0;
+        data[nftId].price = 0;
+        data[nftId].typeItem = TypeItem.NONE;
 
         (bool sent,) = owner.call{value : msg.value}(""); // НАДО СДЕЛАТЬ ПАДЕНИЕ ДЛЯ ТЕСТА!
 
@@ -63,82 +65,78 @@ contract Marketplace is AccessControl {
 
     function cancel(uint nftId) public {
         require(NFT721(_nft).ownerOf(nftId) == msg.sender, "Not owner.");
-        data.availableToBuy[nftId] = 0;
+        data[nftId].price = 0;
+        data[nftId].typeItem = TypeItem.NONE;
     }
 
     function checkPriceBuy(uint nftId) public view returns (uint){
-        return data.availableToBuy[nftId];
+        return data[nftId].price;
     }
 
     function listItemOnAuction(uint nftId) public {
         require(NFT721(_nft).ownerOf(nftId) == msg.sender, "Not owner.");
         require(NFT721(_nft).getApproved(nftId) == address(this), "Not approved.");
-        require(data.auctionSw[nftId] == false, "Auction exist.");
+        require(data[nftId].typeItem == TypeItem.NONE, "Auction exist.");
 
-        data.auctionSw[nftId] = true;
-        data.auctionStartTime[nftId] = block.timestamp;
+        data[nftId].typeItem = TypeItem.AUCTION;
+        data[nftId].stratTime = block.timestamp;
     }
 
     function makeBid(uint nftId, uint price) public {
         require(NFT721(_nft).ownerOf(nftId) != msg.sender, "Is owner.");
-        require(data.auctionSw[nftId] == true, "Auction close.");
-        require(price > data.auctionPrice[nftId], "Small bid.");
-        require(data.auctionLastBider[nftId] != msg.sender, "You are the last bider.");
+        require(data[nftId].typeItem == TypeItem.AUCTION, "Auction close.");
+        require(price > data[nftId].price, "Small bid.");
+        require(data[nftId].maxBider != msg.sender, "You are the last bider.");
         require(ERC20Token(_token).allowance(msg.sender, address(this)) >= price, "Don't have allowance");
         require(ERC20Token(_token).balanceOf(msg.sender) >= price, "Don't have balance");
 
-        data.auctionBids[nftId]++;
-        data.auctionPrice[nftId] = price;
-        data.auctionLastBider[nftId] = msg.sender;
+        data[nftId].maxBid++;
+        data[nftId].price = price;
+        data[nftId].maxBider = msg.sender;
     }
 
     function finishAuction(uint nftId) public {
-        require(data.auctionSw[nftId] == true, "Auction close.");
-        require(data.auctionBids[nftId] >= _minBids, "Few bets.");
-        require(data.auctionStartTime[nftId] + _minAuctionTime <= block.timestamp, "Little time has passed.");
+        require(data[nftId].typeItem == TypeItem.AUCTION, "Auction close.");
+        require(data[nftId].maxBid >= _minBids, "Few bets.");
+        require(data[nftId].stratTime + _minAuctionTime <= block.timestamp, "Little time has passed.");
 
         address owner = NFT721(_nft).ownerOf(nftId);
 
         SafeERC20.safeTransferFrom(
             ERC20Token(_token),
-            data.auctionLastBider[nftId],
+            data[nftId].maxBider,
             owner,
-            data.auctionPrice[nftId]
+            data[nftId].price
         );
 
         ERC721(_nft).safeTransferFrom(
             owner,
-            data.auctionLastBider[nftId],
+            data[nftId].maxBider,
             nftId
         );
 
-        data.auctionSw[nftId] = false;
-        data.auctionBids[nftId] = 0;
-        data.auctionPrice[nftId] = 0;
-        data.auctionStartTime[nftId] = 0;
+        data[nftId].typeItem = TypeItem.NONE;
+        data[nftId].maxBid = 0;
+        data[nftId].price = 0;
+        data[nftId].stratTime = 0;
     }
 
     function cancelAuction(uint nftId) public {
-        require(data.auctionSw[nftId] == true, "Auction close.");
-        require(data.auctionStartTime[nftId] + _minAuctionTime <= block.timestamp, "Little time has passed.");
+        require(data[nftId].typeItem == TypeItem.AUCTION, "Auction close.");
+        require(data[nftId].stratTime + _minAuctionTime <= block.timestamp, "Little time has passed.");
 
-        data.auctionSw[nftId] = false;
-        data.auctionBids[nftId] = 0;
-        data.auctionPrice[nftId] = 0;
-        data.auctionStartTime[nftId] = 0;
+        data[nftId].typeItem = TypeItem.NONE;
+        data[nftId].maxBid = 0;
+        data[nftId].price = 0;
+        data[nftId].stratTime = 0;
     }
-
-    function auctionStatus(uint nftId) public view returns (bool){
-        return data.auctionSw[nftId];
-    }
-
     function auctionPrice(uint nftId) public view returns (uint){
-        return data.auctionPrice[nftId];
+        return data[nftId].price;
     }
 
-    modifier onlyMember() {
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Restricted to members.");
-        _;
-    }
+//    modifier onlyMember() {
+//        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Restricted to members.");
+//        _;
+//    }
 
 }
---
+++ contracts/MarketplaceOld.sol
@@ -0,0 +1,144 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./NFT721.sol";
+import "./ERC20Token.sol";
+import "hardhat/console.sol";
+
+
+contract MarketplaceOld is AccessControl {
+    address private _nft;
+    address private _token;
+    uint private _minAuctionTime;
+    uint private _minBids;
+
+    struct DataStruct {
+        mapping(uint => uint) availableToBuy; // nftId => price
+        mapping(uint => bool) auctionSw; // nftId => bool - включен акцион или нет
+        mapping(uint => uint) auctionPrice; // nftId => price
+        mapping(uint => uint) auctionBids; // nftId => bids
+        mapping(uint => uint) auctionStartTime; // nftId => StartTime
+        mapping(uint => address) auctionLastBider; // nftId => последний ставочник
+    }
+
+    DataStruct private data;
+
+    constructor(address nft, address token, uint minAuctionTime, uint minBids) {
+        _nft = nft;
+        _token = token;
+        _minAuctionTime = minAuctionTime;
+        _minBids = minBids;
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function createItem(address to, string memory metadata) public onlyMember {
+        NFT721(_nft).mint(to, metadata);
+    }
+
+    function listItem(uint nftId, uint price) public {
+        require(NFT721(_nft).ownerOf(nftId) == msg.sender, "Not owner.");
+        require(NFT721(_nft).getApproved(nftId) == address(this), "Not approved.");
+        require(price > 0, "Bad price.");
+        data.availableToBuy[nftId] = price;
+    }
+
+    function buyItem(uint nftId) public payable {
+        address owner = NFT721(_nft).ownerOf(nftId);
+
+        require(NFT721(_nft).getApproved(nftId) == address(this), "Not approved.");
+        require(data.availableToBuy[nftId] > 0, "Not available to buy");
+        require(owner != msg.sender, "Already the owner.");
+        require(msg.value >= data.availableToBuy[nftId], "Bad amount.");
+
+        NFT721(_nft).safeTransferFrom(owner, msg.sender, nftId);
+
+        data.availableToBuy[nftId] = 0;
+
+        (bool sent,) = owner.call{value : msg.value}(""); // НАДО СДЕЛАТЬ ПАДЕНИЕ ДЛЯ ТЕСТА!
+
+        require(sent, "Failed to send Ether");
+    }
+
+    function cancel(uint nftId) public {
+        require(NFT721(_nft).ownerOf(nftId) == msg.sender, "Not owner.");
+        data.availableToBuy[nftId] = 0;
+    }
+
+    function checkPriceBuy(uint nftId) public view returns (uint){
+        return data.availableToBuy[nftId];
+    }
+
+    function listItemOnAuction(uint nftId) public {
+        require(NFT721(_nft).ownerOf(nftId) == msg.sender, "Not owner.");
+        require(NFT721(_nft).getApproved(nftId) == address(this), "Not approved.");
+        require(data.auctionSw[nftId] == false, "Auction exist.");
+
+        data.auctionSw[nftId] = true;
+        data.auctionStartTime[nftId] = block.timestamp;
+    }
+
+    function makeBid(uint nftId, uint price) public {
+        require(NFT721(_nft).ownerOf(nftId) != msg.sender, "Is owner.");
+        require(data.auctionSw[nftId] == true, "Auction close.");
+        require(price > data.auctionPrice[nftId], "Small bid.");
+        require(data.auctionLastBider[nftId] != msg.sender, "You are the last bider.");
+        require(ERC20Token(_token).allowance(msg.sender, address(this)) >= price, "Don't have allowance");
+        require(ERC20Token(_token).balanceOf(msg.sender) >= price, "Don't have balance");
+
+        data.auctionBids[nftId]++;
+        data.auctionPrice[nftId] = price;
+        data.auctionLastBider[nftId] = msg.sender;
+    }
+
+    function finishAuction(uint nftId) public {
+        require(data.auctionSw[nftId] == true, "Auction close.");
+        require(data.auctionBids[nftId] >= _minBids, "Few bets.");
+        require(data.auctionStartTime[nftId] + _minAuctionTime <= block.timestamp, "Little time has passed.");
+
+        address owner = NFT721(_nft).ownerOf(nftId);
+
+        SafeERC20.safeTransferFrom(
+            ERC20Token(_token),
+            data.auctionLastBider[nftId],
+            owner,
+            data.auctionPrice[nftId]
+        );
+
+        ERC721(_nft).safeTransferFrom(
+            owner,
+            data.auctionLastBider[nftId],
+            nftId
+        );
+
+        data.auctionSw[nftId] = false;
+        data.auctionBids[nftId] = 0;
+        data.auctionPrice[nftId] = 0;
+        data.auctionStartTime[nftId] = 0;
+    }
+
+    function cancelAuction(uint nftId) public {
+        require(data.auctionSw[nftId] == true, "Auction close.");
+        require(data.auctionStartTime[nftId] + _minAuctionTime <= block.timestamp, "Little time has passed.");
+
+        data.auctionSw[nftId] = false;
+        data.auctionBids[nftId] = 0;
+        data.auctionPrice[nftId] = 0;
+        data.auctionStartTime[nftId] = 0;
+    }
+
+    function auctionStatus(uint nftId) public view returns (bool){
+        return data.auctionSw[nftId];
+    }
+
+    function auctionPrice(uint nftId) public view returns (uint){
+        return data.auctionPrice[nftId];
+    }
+
+    modifier onlyMember() {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Restricted to members.");
+        _;
+    }
+
+}
